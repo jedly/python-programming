5fbea297829a67808a4ba3a15171ff216da73cbb
Jeevan Jedly <41039144+jedly@users.noreply.github.com>
2018-09-25 20:17:56 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,13 +1,11 @@
 num = 16
 
-# uncomment to take input from the user
-#num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
 if num < 0:
    print("Enter a positive number")
 else:
    sum = 0
-   # use while loop to iterate un till zero
    while(num > 0):
        sum += num
        num -= 1
