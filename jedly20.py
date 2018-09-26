346763c227fc44e3198005611e8630d348b4e53e
Jeevan Jedly <41039144+jedly@users.noreply.github.com>
2018-09-26 22:57:56 +0530

    Update jedly20.py

--- jedly20.py
+++ jedly20.py
@@ -1,9 +1,9 @@
 time = float(input("Input time in seconds: "))
-day = time // (24 * 3600)
+1day = time // (24 * 3600)
 time = time % (24 * 3600)
 hour = time // 3600
 time %= 3600
 minutes = time // 60
 time %= 60
 seconds = time
-print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
+print("d:h:m:s-> %d:%d:%d:%d" % (1day, hour, minutes, seconds))
